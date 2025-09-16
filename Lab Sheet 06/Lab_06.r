ec36601b2f224cb08bbda8a3a204f9c29cc9c0a8
IT24100925 <IT24100925@my.sliit.lk>
2025-09-16 09:59:47 +0530

    Lab_06

---
+++ Lab Sheet 06/Lab_06.r
@@ -0,0 +1,16 @@
+setwd("//Users//hesarapamuditha//SLIIT//Y2S1//P&S//Lab//Lab 06")
+##Q1
+#1.
+#Binomial distribution
+#random variable x has binomial distribution with n=50,p=0.85
+
+#2
+dbinom(47,50,0.85)
+
+##Q2
+#1 Number  of calls received per hour
+
+#2 Poisson distribution
+
+#3
+dpois(15,12)

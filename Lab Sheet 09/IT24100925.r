f3455a15989754a81b456ad72d5838526444e9cb
IT24100925 <IT24100925@my.sliit.lk>
2025-10-01 22:24:45 +0530

    Lab_09

---
+++ Lab Sheet 09/IT24100925.r
@@ -0,0 +1,8 @@
+setwd("//Users//hesarapamuditha//Library//Mobile Documents//com~apple~CloudDocs//Y2S1//P&S//Lab//Lab 09")
+##Exercise
+#1).
+bake <- rnorm(25, mean = 45, sd = 2)
+bake
+
+#2).
+t.test(bake, mu = 46, alternative = "less")
